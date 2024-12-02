f00f6b0fae52eb3665d8690cb7fa8d3a872d5db2
AhmadAlroqi <ahmadyalroqi@gmail.com>
2024-12-02 10:40:23 +0300

    Add files via upload

--- DigLock_A.sv
+++ DigLock_A.sv
@@ -1,58 +1,82 @@
 `timescale 1ns / 1ps
 
+module DigLock_T4(
+    input logic clk,
+    input logic reset_n,
+    input logic [3:0] in, 
+    output logic [2:0] fail_count,
+    output logic z
+);
 
+    typedef enum logic [1:0] {
+        C,         
+        O,         
+        Failed     
+    } state_t;
 
-module DigLock_A(
-input logic clk,
-input logic reset_n,
-input logic [3:0]in,
-output logic [2:0]counter,
-output logic [2:0]counter1,
-output logic z
+    state_t p_state, n_state;
+    logic [2:0] counter;
 
-    );
+    always_ff @(posedge clk or negedge reset_n) begin
+        if (!reset_n)
+            p_state <= C; 
+        else
+            p_state <= n_state; 
+    end
+
+    always_comb begin
+        case (p_state)
+            C: begin
+                if (in == 4'b1101) 
+                    n_state = O; 
+                else if (fail_count == 2)
+                    n_state = Failed; 
+                else
+                    n_state = C; 
+            end
+
+            O: begin
+                n_state = C; 
+            end
+
+            Failed: begin
+                if (counter == 4)
+                    n_state = C; 
+                else
+                    n_state = Failed; 
+            end
+
+            default: n_state = C; 
+        endcase
+    end
+
+    always_ff @(posedge clk or negedge reset_n) begin
+        if (!reset_n) begin
+            fail_count <= 3'b0; 
+            counter <= 3'b0;    
+        end else begin
+            case (p_state)
+                C: begin
+                    if (in != 4'b1101 && fail_count < 3)
+                        fail_count <= fail_count + 1; 
+                    if (fail_count == 3)
+                        fail_count <= 0; 
+                end
+
+                  Failed: begin
+                    if (counter == 4)
+                        counter <= 3'b0; 
+                    else
+                        counter <= counter + 1;
+                end
+                
+
+                default: ; 
+            endcase
+        end
+    end
+
     
- typedef enum logic [2:0]{
- A,B,Filed
- }state_t;   
- 
- state_t p_state,n_state;
- 
- always_ff@(posedge clk, negedge reset_n)
- begin
-  
-  if (!reset_n)
-      p_state<=A;
-  else 
-  p_state<=n_state;    
-  
-  end 
-  
-  always @(in,p_state)
-begin 
+    assign z = (p_state == O); 
 
-case(p_state)
-
-A: begin
-if (in==4'b1101) n_state=B;
-else begin
- n_state=Filed;
-counter <= counter+1; end 
-end
-Filed:
-begin 
-if (counter==2) counter1<=counter+1;
-if(counter1==10000000)
-n_state=A;
-end
-
-
-
-endcase
-end 
-
-assign z=(p_state==B);
-
-   
- 
 endmodule
